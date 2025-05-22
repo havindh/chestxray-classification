e29e6ebbfd8cf6a5614163c6f52b62a89ed1e83b
Havin <43862699+havindh@users.noreply.github.com>
2025-05-21 18:16:44 -0700

    Converted PIL image to numpy array

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -7,6 +7,7 @@ import torchxrayvision as xrv
 from torchvision import transforms
 from PIL import Image
 import logging
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +23,10 @@ def preprocess_xray(img_path, img_size=224):
     torch.Tensor: Preprocessed image tensor of shape [1, img_size, img_size]
     """
     try:
-        # Load as grayscale
+        # Load as grayscale 
+        # This is a PIL image we  should convert it to numpy arrrays
         img = Image.open(img_path).convert('L')
-        
+        img = np.array(img)
         # Use TorchXRayVision's normalization function
         # This handles the conversion to the expected [-1024, 1024] range
         img = xrv.datasets.normalize(img, 255)
